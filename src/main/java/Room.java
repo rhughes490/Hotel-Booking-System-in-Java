53e6acfaf1e7dd00a8ab84193bdb4485cecf4aa6
Ronan Hughes <rhughes490@gmail.com>
2020-07-22 11:09:08 +0100

    Basic Structure complete, started tests on Room class

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Room {
+  private String name;
+  private int capacity;
+  private String type;
+  private ArrayList<Guest> guests;
+  
+  public Room(String name, int capacity, String type) {
+    this.name = name;
+    this.capacity = capacity;
+    this.type = type;
+    this.guests = new ArrayList<Guest>();
+  }
+
+  public int getCapacity() {
+    return capacity;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String type() {
+    return type;
+  }
+
+  public int guestsInRoom() {
+    return this.guests.size();
+  }
+
+
+
 }
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RoomTest {
+
+  private Room room;
+  private Guest guest;
+
+  @Before
+  public void before() {
+    room = new Room("A123", 2, "Double");
+    guest = new Guest("Bob");
+  }
+
+  @Test
+  public void getCapacity() {
+    assertEquals(2, room.getCapacity());
+  }
 }
